8ea24f4d71bea9a4378b0418175a7629b3e7d1d8
Carl <carl.agbenyega@gmail.com>
2020-09-11 23:01:58 -0600

    implemented the drawer and stack navigation for blank home and settings pages

--- App.js
+++ App.js
@@ -1,13 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to UNM Student Resources!</Text>
+      <StatusBar style="auto" />
+      <Button 
+        onPress={() => {navigation.navigate("Settings")}}
+        title="Go to settings page"
+      />
+    </View>
+  );
+}
+
+const SettingsScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text>This will hold the app settings</Text>
+      <StatusBar style="auto" />
+      <Button 
+        onPress={() => {navigation.navigate("Home")}}
+        title="Go to home page"
+      />
+    </View>
+  );
+}
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home" >
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Settings" component={SettingsScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
