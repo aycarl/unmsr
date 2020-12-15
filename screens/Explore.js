9b24e31df1a82ac68194c6206cc3996d8bc6db85
Carl <carl.agbenyega@gmail.com>
2020-12-14 21:31:42 -0700

    Update Explore.js

--- screens/Explore.js
+++ screens/Explore.js
@@ -46,7 +46,7 @@ const styles = StyleSheet.create({
   },
   pageContent: {
     flex: 1,
-    paddingTop: 35,
+    paddingTop: 40,
     paddingLeft: 10,
     paddingRight: 10,
     paddingBottom: 10,
