9539d0696daaa319bf26181329ac2a5408f0bda7
Carl <carl.agbenyega@gmail.com>
2020-12-18 15:47:48 -0700

    added deata persistence
    added react native community asunc storage & redux persist, logger for debugging

---
+++ redux/store.js
@@ -0,0 +1,18 @@
+import { createStore, applyMiddleware } from "redux";
+import { persistStore } from "redux-persist";
+import logger from "redux-logger";
+
+import rootReducer from "./RootReducer";
+
+const middleware = [];
+
+if (process.env.NODE_ENV === "development") {
+  middleware.push(logger);
+}
+
+export const store = createStore(
+  rootReducer,
+  applyMiddleware(...middleware)
+);
+
+export const persistor = persistStore(store);
