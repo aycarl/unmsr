d231a1ab22780ac19abf7d640a469f7963662464
Carl <carl.agbenyega@gmail.com>
2020-12-19 23:48:52 -0700

    added mock data and reselect library
    created selector file for events

--- screens/EventDetails.js
+++ screens/EventDetails.js
@@ -1,18 +1,48 @@
 import React from "react";
 import { View, StyleSheet, Text } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 
-const EventDetails = () => {
+import ScreenHeader from "./../custom-components/ScreenHeader";
+
+const EventDetails = ({ navigation }) => {
   return (
-    <View style={styles.container} >
-      <Text>Event Details Screen!</Text>
+    <View style={styles.container}>
+      <ScreenHeader title="" navigation={navigation} />
+      <ScrollView contentContainerStyle={styles.eventDetailsContainer}>
+        <Text style={styles.eventTitle} >Event Details Screen!</Text>
+        <Text>Hosted by <Text>ORGANIZER</Text></Text>
+        <Text style={styles.textHeaders} >Time</Text>
+        <Text style={styles.textHeaders} >Summary</Text>
+        <Text style={styles.textHeaders} >Description</Text>
+        <Text style={styles.textHeaders} >Categories</Text>
+        <Text style={styles.textHeaders} >Events type / class</Text>
+        <View></View>
+        <Text></Text>
+      </ScrollView>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+  },
+  eventDetailsContainer: {
+    paddingHorizontal: 15,
+    paddingVertical: 15,
+  },
+  eventTitle: {
+    fontWeight: "bold",
+    fontSize: 20,
+    textTransform: "uppercase",
+    marginBottom: 10,
+  },
+  textHeaders: {
+    fontSize: 16,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    marginBottom: 10,
   },
 });
 
