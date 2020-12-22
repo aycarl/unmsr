32114d11674f4372de53d7d2f9339004c11c9d84
Carl <carl.agbenyega@gmail.com>
2020-12-21 20:41:32 -0700

    created organisations selectors and reducer

--- redux/RootReducer.js
+++ redux/RootReducer.js
@@ -3,6 +3,7 @@ import { persistReducer } from "redux-persist";
 import AsyncStorage from "@react-native-community/async-storage";
 
 import eventsReducer from "./events/eventsReducer";
+import orgsReducer from "./orgs/orgsReducer";
 // import userReducer from "./user/userReducer";
 
 const persistConfig = {
@@ -15,6 +16,7 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
   events: eventsReducer,
+  orgs: orgsReducer,
 });
 
 export default persistReducer(persistConfig, rootReducer);
---
+++ redux/orgs/orgsReducer.js
@@ -0,0 +1,15 @@
+//import OrgActionTypes from "./orgActionTypes";
+
+const INITIAL_STATE = {
+  orgsList: {}
+}
+
+const orgsReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  
+    default:
+      return state;
+  }
+}
+
+export default orgsReducer;
