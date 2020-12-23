abfd139c80ab79f9d9694cb91afb0319fdec00d2
Carl <carl.agbenyega@gmail.com>
2020-12-23 16:59:17 -0700

    clean up and component updates

--- custom-components/event-components/EventPreviewCard.js
+++ custom-components/event-components/EventPreviewCard.js
@@ -17,7 +17,7 @@ const styles = StyleSheet.create({
   image: {
     flex: 1,
     width: 300,
-    resizeMode: "contain",
+    // resizeMode: "contain",
     borderColor: "#707070",
     borderRadius: 5,
     borderWidth: 1,
--- navigation-containers/ExploreNavContainer.js
+++ navigation-containers/ExploreNavContainer.js
@@ -2,15 +2,23 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import Explore from "./../screens/Explore";
+import OrgDetails from "./../screens/OrgDetails";
 
 const Stack = createStackNavigator();
 
 const ExploreNavContainer = () => {
-  return(
-    <Stack.Navigator initialRouteName="ExploreHome"  headerMode="none">
+  return (
+    <Stack.Navigator initialRouteName="OrgDetails" headerMode="none">
       <Stack.Screen name="ExploreHome" component={Explore} />
+      <Stack.Screen
+        name="OrgDetails"
+        component={OrgDetails}
+        options={{
+          headerShown: true,
+        }}
+      />
     </Stack.Navigator>
   );
-}
+};
 
 export default ExploreNavContainer;
