db2e795d33e023cee7d630b1c77181fc42dea141
Carl <carl.agbenyega@gmail.com>
2020-12-24 21:01:48 -0700

    Update orgsSelectors.js
    created selector for picking list of organization for preview

--- redux/orgs/orgsSelectors.js
+++ redux/orgs/orgsSelectors.js
@@ -2,11 +2,27 @@ import { createSelector } from "reselect";
 
 const selectOrgs = state => state.orgs;
 
-// select the list of organizations from state
+// select all organizations from state as object
 export const selectOrgsList = createSelector(
   [selectOrgs],
   orgs => orgs.orgsList 
 );
+
+// select all organisations (UID, name & number of members) from state as array
+export const selectOrgsListForPreview = createSelector(
+  [selectOrgsList],
+  orgsList => orgsList ? Object.keys(orgsList).map(orgUID => {
+    const { UID, name, nickname, members } = orgsList[orgUID];
+    return ({
+      UID,
+      name,
+      nickname,
+      memberCount: members.length,
+    });
+  }) : []
+);
+
+// TODO: create selector for list of organisation that a use is a part of!!!
 
 // select the list of organization categories from state
 export const selectCategoryList = createSelector(
