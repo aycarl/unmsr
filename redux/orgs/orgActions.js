4bc85e8de18d86f0fe6068679cbac4933858161a
Carl <carl.agbenyega@gmail.com>
2020-12-26 16:15:13 -0700

    created user action, reducer, action types, defined state and edited org actions

--- redux/orgs/orgActions.js
+++ redux/orgs/orgActions.js
@@ -1,6 +1,19 @@
 import OrgActionTypes from "./orgActionTypes";
 
-export const UpdatedExploreSearchQuery = query => ({
+// explore action to search organisation list
+export const updatedExploreSearchQuery = query => ({
   type: OrgActionTypes.UPDATE_EXPLORE_SEARCH_QUERY,
   payload: query
+});
+
+// org action to remove user from org member list
+export const removeMember = userID => ({
+  type: OrgActionTypes.REMOVE_MEMBER,
+  payload: userID
+});
+
+// org actiont to add user to member list
+export const addMember = userID => ({
+  type: OrgActionTypes.ADD_NEW_MEMBER,
+  payload: userID
 });
