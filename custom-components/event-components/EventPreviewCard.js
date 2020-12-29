f2c45a17f63caa8bc411fbb46664f621d2640ccd
Carl <carl.agbenyega@gmail.com>
2020-12-29 11:45:01 -0700

    updated event preview card & container on home screen

--- custom-components/event-components/EventPreviewCard.js
+++ custom-components/event-components/EventPreviewCard.js
@@ -1,28 +1,64 @@
 import React from "react";
+import { useNavigation } from "@react-navigation/native";
 import { View, Text, StyleSheet, ImageBackground } from "react-native";
+import { TouchableHighlight } from "react-native-gesture-handler";
+
 
 // TODO: takes an image url and a url to Update page as props
-const EventPreviewCard = () => {
+const EventPreviewCard = ({ SUMMARY, DTSTART, UID}) => {
+  const navigation = useNavigation();
+
+  console.log("event UID for preview card: "+JSON.stringify(UID))
+
   return (
-    <View>
-      <ImageBackground 
+    <TouchableHighlight
+      style={styles.container}
+      activeOpacity={0.8}
+      underlayColor="#fff"
+      onPress={() => {
+        navigation.navigate("EventDetails", {
+          eventID: UID,
+        });
+      }}
+    >
+      <ImageBackground
         style={styles.image}
         source={require("./../../assets/updates/PNMGC_Notice.png")}
-      ></ImageBackground>
-    </View>
+      >
+        <Text style={styles.eventName} numberOfLines={1} ellipsizeMode={"tail"}>
+          {SUMMARY} Some event name
+        </Text>
+        <Text style={styles.eventTime} numberOfLines={1} ellipsizeMode={"tail"}>
+          {DTSTART} Some time
+        </Text>
+      </ImageBackground>
+    </TouchableHighlight>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  image: {
+  container: {
     flex: 1,
     width: 300,
-    // resizeMode: "contain",
     borderColor: "#707070",
     borderRadius: 5,
     borderWidth: 1,
     marginHorizontal: 10,
     overflow: "hidden",
+  },
+  image: {
+    flex: 1,
+    padding: 7,
+    alignItems: "flex-start",
+    justifyContent: "flex-end",
+  },
+  eventName: {
+    fontWeight: "bold",
+    color: "#707070",
+    textTransform: "uppercase",
+  },
+  eventTime: {
+    color: "#707070",
   },
 });
 
