840768f599d69f7bb19ea78238cd82d795066391
Carl <carl.agbenyega@gmail.com>
2020-12-30 19:31:51 -0700

    created utils file for formating UNM Open data

--- screens/EventDetails.js
+++ screens/EventDetails.js
@@ -1,43 +1,46 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, Linking } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 
-import {
-  selectEvent,
-} from "./../redux/events/eventsSelectors";
+import { selectEvent } from "./../redux/events/eventsSelectors";
+
+import { convertStringToReadableDateTime, extractEmailAndMailToLink } from "./../utils/dateUtils";
 
 const EventDetails = (props) => {
-
   const eventInfo = useSelector(selectEvent(props.route.params.eventID));
 
-  // to debug: 
-  console.log(JSON.stringify("Event UID: "+ props.route.params.eventID));
-  console.log(JSON.stringify("Event Details: "+ JSON.stringify(eventInfo)));
+  // to debug:
+  console.log(JSON.stringify("Event UID: " + props.route.params.eventID));
+  console.log(JSON.stringify("Event Details: " + JSON.stringify(eventInfo)));
+
+  let startDateTime = convertStringToReadableDateTime(eventInfo.DTSTART).fullUTCDateString;
+  let endDateTime = convertStringToReadableDateTime(eventInfo.DTEND).fullUTCDateString;
+  const { emailAddress, mailToLink } = extractEmailAndMailToLink(eventInfo.ORGANIZER);
 
   return (
     <View style={styles.container}>
       <ScrollView contentContainerStyle={styles.eventDetailsContainer}>
         <Text style={styles.eventTitle}>{eventInfo.SUMMARY}</Text>
         <Text>
-          Hosted by <Text>{eventInfo.ORGANIZER}</Text>
+          Hosted by{" "}
+          <Text
+            style={styles.textLink}
+            onPress={() => Linking.openURL(mailToLink)}
+          >
+            {emailAddress}
+          </Text>
         </Text>
-        <Text style={styles.textHeaders}>Time</Text>
-        <Text>
-          {eventInfo.DTSTART}
-        </Text>
+        <Text style={styles.textHeaders}>Start Time</Text>
+        <Text>{startDateTime}</Text>
+        <Text style={styles.textHeaders}>End Time</Text>
+        <Text>{endDateTime}</Text>
         <Text style={styles.textHeaders}>Description</Text>
-        <Text>
-          {eventInfo.DESCRIPTION}
-        </Text>
+        <Text>{eventInfo.DESCRIPTION}</Text>
         <Text style={styles.textHeaders}>Categories</Text>
-        <Text>
-          {eventInfo.CATEGORIES}
-        </Text>
-        <Text style={styles.textHeaders}>Event type / class</Text>
-        <Text>
-          {eventInfo.CLASS}
-        </Text>
+        <Text>{eventInfo.CATEGORIES}</Text>
+        <Text style={styles.textHeaders}>Event type</Text>
+        <Text>{eventInfo.CLASS}</Text>
         <View></View>
         <Text></Text>
       </ScrollView>
@@ -65,6 +68,9 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textTransform: "uppercase",
     marginVertical: 10,
+  },
+  textLink: {
+    color: "#ba0c2f",
   },
 });
 
