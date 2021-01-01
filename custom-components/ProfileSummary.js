b166c74c6b16bbead5a51c33b32e13392c0b19cb
Carl <carl.agbenyega@gmail.com>
2021-01-01 00:51:25 -0700

    added firebase sign up, log in and log out actions

--- custom-components/ProfileSummary.js
+++ custom-components/ProfileSummary.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import { View, Text, StyleSheet, Image } from "react-native";
 
 import { selectFirstName, selectLastName, selectEmailAddress, selectPhoneNumber, selectEducationLevel } from "./../redux/user/userSelectors";
