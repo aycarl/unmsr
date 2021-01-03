62f78759463fcf15f9fa394e09b59efa37b94898
Carl <carl.agbenyega@gmail.com>
2021-01-03 13:13:16 -0700

    added top tab navigation for events section

---
+++ navigation-containers/EventsTopNavContainer.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+
+import Events from "./../screens/Events";
+import MyOrgEvents from "./../screens/MyOrgEvents";
+
+// creates Tab navigation for events
+const Tab = createMaterialTopTabNavigator();
+
+// Navigation container for Event list screens
+const EventsTopNavContainer = () => {
+  return (
+    <Tab.Navigator initialRouteName="" lazy tabBarOptions={{
+      labelStyle: {fontSize: 14, color: "#ffffff", fontWeight: "bold"},
+      tabStyle: { marginTop: 20 },
+      style: { backgroundColor: "#ba0c2f" },
+      indicatorStyle: { backgroundColor: "#ffffff" }
+    }}>
+      <Tab.Screen
+        name="MyOrgEvents"
+        component={MyOrgEvents}
+        options={{
+          headerShown: false,
+          title: "My Events",
+        }}
+      />
+      <Tab.Screen
+        name="EventsHome"
+        component={Events}
+        options={{
+          headerShown: false,
+          title: "All UNM Events",
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default EventsTopNavContainer;
